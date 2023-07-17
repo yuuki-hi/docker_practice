599df32a4ea50c1d55ec594d4af7cb236946f03c
yuuki-hi <bino723ydk@icloud.com>
2023-07-17 20:20:17 +0900

    change

--- flask/app/index.py
+++ flask/app/index.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "<h1>Hello, Flask!!!!!!!!!!!!!</h1>"
+  return "<h1>Hello, Flask!!!</h1>"
+
+@app.route("/api/test")
+def test():
+  return "<h1>Hello, API!</h1>"
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=80, debug=True)
